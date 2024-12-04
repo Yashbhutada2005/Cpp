5e468e3cba32c3f35e833e5761a28ad44d37abc1
Yashbhutada2005 <yashbhutada05@gmail.com>
2024-12-04 19:05:39 +0530

    .

---
+++ Basics/data_types.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main() {
+ 
+ // built in data types
+
+ /* int = numbers
+    float = decimal number
+    char = character , symbol
+    double = long digit number
+    boolean = true or false
+*/ 
+
+// user defined 
+
+/* struct
+   union
+   enum
+*/
+
+// derived data type
+
+/* array
+   function
+   pointer
+*/
+
+
+    int a = 1;
+    float b = 2.345;
+    char c = '*';
+    double d = 237537;
+    bool e = true;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+
+    return 0;
+
+
+
+
+}
